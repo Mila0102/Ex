c9d8d7c6561312ec29256d1160ba4e0a64107b20
Mila <milarest01@gmail.com>
2023-12-03 14:58:25 +0700

    save

---
+++ Examples/Example/Sem2_1/Program.cs
@@ -0,0 +1,24 @@
+﻿//случайное число из отрезка 10 до 99 и показ нибольшее
+
+int number = GetNumber();
+
+int num1 = number / 10; //78/10=7
+int num2 = number % 10;//78%10 =8
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
+
+
+int GetNumber()
+{
+    Random rand = new Random();
+    int num = rand.Next(10, 100);
+
+    return num;
+}

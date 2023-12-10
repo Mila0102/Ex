a6e4e15ab80227b65529f552b0a68a4318304c2f
Mila <milarest01@gmail.com>
2023-12-10 13:25:12 +0700

    save

---
+++ Examples/Example/Work1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход координаты точки 
+//(X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, 
+//в которой находится эта точка.
+
+Console.Write("Введите x");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ((x > 0) && (y > 0))
+{
+    Console.Write("1");
+}
+if ((x < 0) && (y > 0))
+{
+    Console.Write("2");
+}
+if ((x < 0) && (y < 0))
+{
+    Console.Write("3");
+}
+if ((x > 0) && (y < 0))
+{
+    Console.Write("4");
+}
---
+++ Examples/Example/Work1_4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает натуральное число N, 
+//а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите натуральное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string numberString = number.ToString();
+char[] charArray = numberString.ToCharArray();
+
+for (int i = 0; i < charArray.Length; i++)
+{
+   Console.Write($"{charArray[i]},\t");
+}
+
+
+
+

0defd85a6fe016ab77dfa4790ba026e8e786abda
Mila <milarest01@gmail.com>
2023-12-21 20:20:18 +0700

    save

---
+++ Examples/Example/SemStr2/Program.cs
@@ -0,0 +1,20 @@
+﻿// На основе символов строки (тип string) сформировать 
+// массив символов (тип char[]). Вывести массив на 
+// экран. Указание Метод строки ToCharArray() не 
+// использовать. 
+// Пример “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
+
+string str = "Hello";
+char[] array = new char[str.Length];
+
+for (int i = 0; i < str.Length; i++)
+{
+    array [i] =  str[i];
+    
+}
+Console.Write('[');
+for (int l = 0; l < array.Length; l++)
+{
+    Console.WriteLine($"'{array[l]}', ");
+}
+Console.Write(']');

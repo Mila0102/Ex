1ac51ee47ad6556e9c5b7adfd8079ba29d932d6e
Mila <milarest01@gmail.com>
2023-12-21 20:33:49 +0700

    save

---
+++ Examples/Example/SemStr3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: 
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+
+int[,] arr = new int[5, 5];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = i + j;
+    }
+    
+}
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write($"{arr[i, j]}, ");
+    }
+    Console.WriteLine();
+}

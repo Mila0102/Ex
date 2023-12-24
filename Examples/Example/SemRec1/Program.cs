ae77761fa514f1f3e94439ee10268f15932d0898
Mila <milarest01@gmail.com>
2023-12-24 15:35:03 +0700

    save

---
+++ Examples/Example/SemRec1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значение N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке 
+// от 1 до N.
+
+int N = GetNumber();
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число N ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void listNumber(int num, int start = 1)
+{
+    
+    if(start <=N)
+    {
+        Console.WriteLine(start);
+        listNumber(num, start+1);
+    }
+    
+}
+
+listNumber(N);
+
+
+

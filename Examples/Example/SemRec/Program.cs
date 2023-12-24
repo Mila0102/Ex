8d4f941b50dc1bb9af07bcbdd52caf5b7afc213c
Mila <milarest01@gmail.com>
2023-12-24 15:12:48 +0700

    save

---
+++ Examples/Example/SemRec/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая будет принимать на 
+// вход число и возвращать сумму его цифр.
+
+int number = GetNumber();
+
+
+int GetNumber()
+{
+    Console.Write("Введите число ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int sum = FindSum(number);
+
+int FindSum(int number)
+{
+    if(number == 0) 
+    {
+        return 0;
+    }
+
+   return number%10 + FindSum(number/10);
+    
+}
+
+Console.Write(sum);

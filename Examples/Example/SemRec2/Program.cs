2d4769a97ae81f43dcc4383738c0ae0468b6f64f
Mila <milarest01@gmail.com>
2024-01-03 11:26:08 +0700

    save

---
+++ Examples/Example/SemRec2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Считать строку с консоли, содержащую латинские 
+// буквы. Вывести на экран согласные буквы этой строки.
+
+string str = getstr();
+
+PrintString(str);
+string getstr() 
+{
+    Console.WriteLine("Введите строку ");
+    string str = Console.ReadLine();
+    return str;
+}
+
+
+bool IsVowel(char c)
+    {
+        return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+
+    }
+
+void PrintString(string str, int index=0)
+{
+
+    if(index < str.Length)
+    {
+      char c = Char.ToUpper(str[index]);
+
+      if(c >= 'A' && c<= 'Z') && (IsVowel c)
+      {
+        Console.Write(c + " ");
+      }
+      PrintString(str, index, +1);
+    }
+
+}

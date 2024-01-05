83abec865e6aca72a27adfcd144611122f518e49
Mila <milarest01@gmail.com>
2024-01-05 23:07:29 +0700

    save

---
+++ Examples/Example/HomeWorkRec2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана 
+// c помощью рекурсии. Даны два неотрицательных числа m и n.
+using System;
+
+class Program
+{
+  static void Main(string[] args)
+   {
+    int m = 2;
+    int n = 1;
+    int result = FuncAck(m, n);
+    Console.WriteLine($"{result}");
+   }
+
+  static int FuncAck(int m, int n)
+   {
+    if(m == 0)
+    {
+        return n + 1;
+    }
+    else if((m > 0) && (n == 0))
+    {
+        return FuncAck(m-1, 1);
+    }
+    else if((m > 0) && (n > 0))
+    {
+        return FuncAck(m - 1, FuncAck(m, n - 1));
+    }
+    else 
+    {
+        return -1;
+    }
+   }
+
+}
